b12422f1fa22aa70d9384d301687bd693d81d57c
BidhanCR <bidhanroy300805@gmail.com>
2024-05-20 19:27:19 +0600

    getRestaurant details api created

--- src/routes/RestaurantRoute.ts
+++ src/routes/RestaurantRoute.ts
@@ -4,7 +4,10 @@ import RestaurantController from "../controllers/RestaurantController";
 
 
 const router = express.Router();
-router.get("/search/:city", param("city").isString().trim().notEmpty().withMessage("City paramenter must be a valid string"), RestaurantController.searchRestaurants);
+
+router.get("/:restaurantId", param("city").isString().trim().notEmpty().withMessage("City parameter must be a valid string"), RestaurantController.getRestaurant)
+
+router.get("/search/:city", param("city").isString().trim().notEmpty().withMessage("City parameter must be a valid string"), RestaurantController.searchRestaurants);
 
 
 export default router;
