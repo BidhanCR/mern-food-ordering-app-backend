755609d5e69bb128e48ac6c5a5e95994b063e928
BidhanCR <bidhanroy300805@gmail.com>
2024-05-21 14:42:47 +0600

    order api created

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,25 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/myUserRoutes";
 import myRestaurantRoute from "./routes/myRestaurantRoute";
-import RestaurantRoute from "./routes/RestaurantRoute"
-import {v2 as cloudinary} from "cloudinary"
+import RestaurantRoute from "./routes/RestaurantRoute";
+import { v2 as cloudinary } from "cloudinary";
+import OrderRoute from "./routes/OrderRoute";
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
   .then(() => console.log("Connected to database!"));
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-})
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 const app = express();
-app.use(express.json());
+
 app.use(cors());
+
+app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
+app.use(express.json());
 
 app.get("/health", async (req: Request, res: Response) => {
   res.send({ message: "Health OK!" });
@@ -27,6 +31,7 @@ app.get("/health", async (req: Request, res: Response) => {
 app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", myRestaurantRoute);
 app.use("/api/restaurant", RestaurantRoute);
+app.use("/api/order", OrderRoute);
 
 app.listen(7000, () => {
   console.log("server started on localhost:7000");
